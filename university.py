b5454b65be762c53d4e813f95d8fc38558bb761d
BraeWebb <email@braewebb.com>
2017-01-10 21:06:42 +1000

    Flask Access Update
    Updated the flask serving model to reflect changes made to the data models
    and to serve the template assessment index file with the appropriate
    data.
    
    Also added a new context utility - type_of - used to determine the type of
    an object in the jinja2 environment.

--- university.py
+++ university.py
@@ -1,15 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from assessment import Assessment, Category
 
 app = Flask(__name__)
 
 
 @app.route('/assessments')
 def view_assessments():
-    return 'An index of assessments with information regarding their completion, work, due date, course and result'
+    return render_template('assessments.html', grouping=sorted(Category.get_all() + Assessment.get_nocategory()))
 
 @app.route('/results')
 def view_results():
     return 'A page containing many different statistics about assessment results'
+
+@app.context_processor
+def custom_context_utils():
+    return dict(type_of=lambda item: type(item).__name__)
 
 
 if __name__ == '__main__':

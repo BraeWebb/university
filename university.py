5e5ebf461a0d28febddabd8bfaad01d4ae2362ab
BraeWebb <email@braewebb.com>
2017-01-20 18:43:53 +1000

    Added Courses Page
    Added a course page which allows courses to be viewed based on the
    semester they were undertaken
    Updated the university.py file to serve the /courses url to the
    courses.html template. Also added a route for handling individual courses
    Created a courses.html template file with the following features
    - A link to course creation in the page header
    - A list of courses separated by semester which includes their code, title
      and a link to their individual page

--- university.py
+++ university.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 from assessment import Assessment, Category
+from course import Course
 
 app = Flask(__name__)
 
@@ -12,6 +13,14 @@ def view_assessments():
 @app.route('/results')
 def view_results():
     return 'A page containing many different statistics about assessment results'
+
+@app.route('/courses')
+def view_courses():
+    return render_template('courses.html', semesters=['2017S1', '2016S2', '2016S1'], Course=Course)
+
+@app.route('/course/<course>')
+def view_course(course):
+    return render_template('course.html', course=Course(course))
 
 @app.context_processor
 def custom_context_utils():

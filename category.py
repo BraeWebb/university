25b3a1af992b0f8cb3f74cbd31db53f27fe1e512
BraeWebb <email@braewebb.com>
2017-01-23 09:09:23 +1000

    Category Update
    Added the done and result variables to the category class which represent
    whether all assessment in a category is done and the total result of all
    assessments
    
    Changed date for when the category is done to represent the due date of
    the latest assessment
    
    Added the create method to add categories to the database programatically
    Added the delete method to delete categories from the database
    
    Allowed for getting assessment based on the done or not done status of the
    assessments
    
    Added a representation of the category to allow for easy debugging

--- category.py
+++ category.py
@@ -1,6 +1,7 @@
 from database import database
 from assessment import Assessment
 from course import Course
+from datetime import datetime
 
 class Category(object):
     def __init__(self, category_id):
@@ -9,11 +10,33 @@ class Category(object):
             if db.exists('categories', category_id = self.category_id):
                 self.course, self.title = \
                     db.query('SELECT course, title FROM categories WHERE category_id = %s', self.category_id)[0]
-                self.worth = db.query('SELECT SUM(worth) FROM assessments WHERE category = %s', self.category_id)[0][0]
-                self.due = db.query('SELECT MIN(due) FROM assessments WHERE category = %s', self.category_id)[0][0]
+                if db.exists('assessments', category = self.category_id):
+                    self.worth = db.query('SELECT SUM(worth) FROM assessments WHERE category = %s', self.category_id)[0][0]
+                    self.done = not db.exists('assessments', category = self.category_id, done = False)
+                    self.result = db.query('SELECT SUM(result*worth) FROM assessments WHERE category = %s', self.category_id)[0][0]/self.worth
+                    if not self.done:
+                        self.due = db.query('SELECT MIN(due) FROM assessments WHERE category = %s AND not done', self.category_id)[0][0]
+                    else:
+                        self.due = db.query('SELECT MAX(due) FROM assessments WHERE category = %s', self.category_id)[0][0]
+                else:
+                    self.result, self.done, self.worth = 0, 0, 0
+                    self.due = datetime.date(datetime.now())
+
+    @classmethod
+    def create(cls, title, course):
+        with database() as db:
+            id = db.query('SELECT MAX(category_id) FROM categories')[0][0] + 1
+            db.query(
+                'INSERT INTO categories (category_id, title, course) VALUES (%s, %s, %s)',
+                id, title, course)
+            return cls(id)
+
+    def delete(self):
+        with database() as db:
+            db.query('DELETE FROM categories WHERE category_id = %s', self.category_id)
 
     @staticmethod
-    def get_all(semester='2016S2'):
+    def get_all(semester='2017S1'):
         with database() as db:
             if not semester:
                 return [Category(cat[0]) for cat in db.query('SELECT category_id FROM categories')]
@@ -35,9 +58,21 @@ class Category(object):
     def get_due(self):
         return self.due
 
-    def get_assessment(self, done=True):
+    def get_result(self):
+        return self.result
+
+    def get_done(self):
+        return self.done
+
+    def get_assessment(self, done=None):
         with database() as db:
-            return sorted([Assessment(assess[0]) for assess in db.query('SELECT assessment_id FROM assessments WHERE category = %s AND done = %s', self.category_id, done)])
+            if done == None:
+                return sorted([Assessment(assess[0]) for assess in db.query('SELECT assessment_id FROM assessments WHERE category = %s', self.category_id)])
+            else:
+                return sorted([Assessment(assess[0]) for assess in db.query('SELECT assessment_id FROM assessments WHERE category = %s AND done = %s', self.category_id, done)])
 
     def __lt__(self, other):
         return self.get_due() < self.get_due()
+
+    def __repr__(self):
+        return 'Category #{}: {}'.format(self.category_id, self.title)

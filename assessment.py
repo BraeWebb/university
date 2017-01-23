df6d3f48739d40e15a210e5d81b4cd38f005d95f
BraeWebb <email@braewebb.com>
2017-01-24 09:22:32 +1000

    Assessment Update
    Added the category class variable to the assessment class which can be
    None if the assessment does not belong in a category
    
    Added a create class method to allow assessment to be added to the
    database, likewise added a delete method to do the opposite
    
    Added set_result and set_done methods to allow for updating the result and
    the completion status through the assessments list
    
    Added a dict method to allow for a proper REST API in the future

--- assessment.py
+++ assessment.py
@@ -6,11 +6,23 @@ class Assessment(object):
         self.assessment_id = int(assessment_id)
         with database() as db:
             if db.exists('assessments', assessment_id = self.assessment_id):
-                self.title, self.worth, self.due, self.course, self.result, self.done = \
-                    db.query('SELECT title, worth, due, course, result, done FROM assessments WHERE assessment_id = %s', self.assessment_id)[0]
+                self.title, self.worth, self.due, self.course, self.result, self.done, self.category = \
+                    db.query('SELECT title, worth, due, course, result, done, category FROM assessments WHERE assessment_id = %s', self.assessment_id)[0]
+
+    @classmethod
+    def create(cls, due, title, worth, course, category):
+        with database() as db:
+            id = db.query('SELECT MAX(assessment_id) FROM assessments')[0][0] + 1
+            db.query('INSERT INTO assessments (assessment_id, title, worth, due, course, result, done, category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
+                     id, title, worth, due, course, 0, False, category if category else None)
+            return cls(id)
+
+    def delete(self):
+        with database() as db:
+            db.query('DELETE FROM assessments WHERE assessment_id = %s', self.assessment_id)
 
     @staticmethod
-    def get_all(semester='2016S2'):
+    def get_all(semester='2017S1'):
         with database() as db:
             if not semester:
                 return sorted([Assessment(assess[0]) for assess in db.query('SELECT assessment_id FROM assessments')])
@@ -18,7 +30,7 @@ class Assessment(object):
                 return sorted([Assessment(assess[0]) for assess in db.query('SELECT assessment_id FROM assessments WHERE course IN (SELECT course FROM courses WHERE semester = %s)', semester)])
 
     @staticmethod
-    def get_nocategory(semester='2016S2', done=True):
+    def get_nocategory(semester='2017S1', done=None):
         with database() as db:
             if done == None:
                 if not semester:
@@ -55,8 +67,34 @@ class Assessment(object):
     def get_result(self):
         return self.result
 
+    def set_result(self, result):
+        with database() as db:
+            db.query('UPDATE assessments SET result = %s WHERE assessment_id = %s', result, self.assessment_id)
+
     def get_done(self):
         return self.done
+
+    def set_done(self, done):
+        with database() as db:
+            db.query('UPDATE assessments SET done = %s WHERE assessment_id = %s', done, self.assessment_id)
+
+    def get_category(self):
+        if self.category:
+            from category import Category
+            return Category(self.category)
+        else:
+            return None
+
+    def dict(self):
+        return {
+            'id': self.assessment_id,
+            'title': self.title,
+            'worth': self.worth,
+            'due': self.due,
+            'course': self.course,
+            'result': self.result,
+            'done': self.result
+        }
 
     def __lt__(self, other):
         return self.get_due() < other.get_due()
